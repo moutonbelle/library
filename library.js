2d1437697103b30655fbe7d5f4c70664468ee0e3
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-07 20:55:57 -0500

    Add html, css; add display book cards on page

--- library.js
+++ library.js
@@ -11,6 +11,7 @@ Book.prototype.info = function () {
 };
 
 const myLibrary = [];
+const libraryDisplay = document.querySelector("#library");
 
 function addBookToLibrary (title, author, pages, read) {
     let newBook = new Book (title, author, pages, read);
@@ -18,5 +19,28 @@ function addBookToLibrary (title, author, pages, read) {
 }
 
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1254, true);
+addBookToLibrary("Harry Potter", "J.K. Rowling", 4562, true);
+addBookToLibrary("The Wandering Inn", "Pirateaba", 10572, false);
 
-console.log(myLibrary);
+myLibrary.forEach((book) => {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book");
+
+    Object.keys(book).forEach(property => {
+        let newProperty = document.createElement("p");
+        newProperty.classList.add("book-property");
+
+        let newPropertyName = document.createElement("span");
+        newPropertyName.classList.add("book-property-name");
+        newPropertyName.textContent = property + ":";
+
+        let newPropertyValue = document.createElement("span");
+        newPropertyValue.classList.add("book-property-value");
+        newPropertyValue.textContent = " "+ book[property];
+
+        newProperty.append(newPropertyName, newPropertyValue);
+        bookCard.append(newProperty);
+    });
+
+    libraryDisplay.appendChild(bookCard);
+});
